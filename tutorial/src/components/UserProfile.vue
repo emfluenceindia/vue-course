360fbd89b49ee86861dba6c64ea723d7ee25cace
Subrata Sarkar <ssarkar@emfluence.com>
2020-10-27 19:37:37 +0530

    Vue looping (v-for) and v-bind directive
    Implemented looping on nested Javascript object using v-for directive for the first time. Used v-bind directive to bind data as html attributes like href and title.

--- tutorial/src/components/UserProfile.vue
+++ tutorial/src/components/UserProfile.vue
@@ -5,11 +5,18 @@
             <div class="user-profile__admin-badge" v-if="user.isAdmin">
                 Admin
             </div>
-            <div class="user-profile__admin-badge" v-if="followers > 100">
-                100+ Followers
-            </div>
             <div class="user-profile__follower-count">
                 Followers: {{followers}}
+           </div>
+        </div>
+        <div class="user-profile__user-posts" v-if="user.posts.length > 0">
+            <h1 class="recent-posts">Recent Posts</h1>
+            <div class="user-profile__post" v-for="post in user.posts" :key="post.id">
+                <h3>{{ post.title }}</h3>
+                <p>{{ post.excerpt }}</p>
+                <span v-bind:title="post.title" class="post-url">
+                    <a target="_blank" v-bind:href="post.url">{{ post.url }}</a>
+                </span>
             </div>
         </div>
     </div>
@@ -21,6 +28,7 @@ export default {
     data() {
         return {
                 followers: 105,
+                post_count: 0,
                 user: {
                     id: 1,
                     username: 'emfl_api',
@@ -28,8 +36,28 @@ export default {
                     firstName: 'Subrata',
                     lastName: 'Sarkar',
                     email: 'subrata@emfluence.com',
-                    isAdmin: true
-                }
+                    isAdmin: true,
+                    posts: [
+                    {
+                        id: 1,
+                        title: 'Traditional Cultural Expression: Durga Puja as an Intangible Cultural Heritage',
+                        excerpt: "The definition of tangible cultural heritage is of physical artefacts produced, maintained and transmitted intergenerationally in society, while intangible...",
+                        url: 'https://ipgyan.com/traditional-cultural-expression-durga-puja-as-an-intangible-cultural-heritage/'
+                    },
+                    {
+                        id: 2,
+                        title: 'Business Requirements in the Modern Digital Age',
+                        excerpt: "India is the land of festivals, and if you are Bengali, then the first thing that comes...",
+                        url: 'https://ipgyan.com/business-requirements-in-the-modern-digital-age/'
+                    },
+                    {
+                        id: 3,
+                        title: 'Intellectual Property Law Considerations of Protecting Laal Paar Shada Shadi',
+                        excerpt: "Pop culture has revived the Bengali way of life back to its screen and made it relevant again...",
+                        url: 'https://ipgyan.com/intellectual-property-law-considerations-of-protecting-laal-paar-shada-shadi/'
+                    },
+                ],
+                },
             }
     },
     computed: {
@@ -52,12 +80,13 @@ export default {
 
     methods: {
         followUser() {
-        this.followers++;
+            this.followers++;
         }
     },
     
     mounted() {
         this.followUser();
+        //this.post_count = this.user.posts.length;
     }
 }
 </script>
@@ -66,7 +95,7 @@ export default {
 .user-profile {
     display: grid;
     grid-template-columns: 1fr 3fr;
-    width: 100%;
+    width: 90%;
     padding:50px 5%;
 }
 
@@ -104,6 +133,41 @@ h1 {
     padding: 4px 10px;
     border-radius: 3px;
 
+}
+
+.user-profile__post {
+    border: 1px solid #989898;
+    text-align: left;
+    border-radius: 5px;
+    margin-bottom: 5px;
+    padding: 6px;
+}
+
+.user-profile__post h3 {
+    font-weight: normal;
+    font-size: 14px;
+    margin: 0 auto;
+}
+
+.user-profile__post p {
+    color: gray;
+    font-size: 12px;
+    margin: 6px auto;
+}
+
+h1.recent-posts {
+    font-size: 18px;
+    margin-bottom: 8px;
+    background-color: rebeccapurple;
+    color: white;
+    padding: 5px 10px;
+    border-radius: 5px;
+}
+
+.post-url a {
+    color: blueviolet;
+    font-size: 12px;
+    text-decoration: none;
 }
 
 

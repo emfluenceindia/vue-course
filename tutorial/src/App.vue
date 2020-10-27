df468feeea90d5cc54baa59a1137688903a8b150
Subrata Sarkar <ssarkar@emfluence.com>
2020-10-27 18:01:30 +0530

    Added first user object
    Added first static user object. Implemented dynamic record display,
     and method triggering. Used computed and watch properties.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>{{fullName}} (@{{user.username}})</div>
+    <span class="followers">Followers: {{followers}}</span>
+    <button @click="followUser">Follow {{user.firstName}}</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'emfl_api',
+        url: 'https://twitter.com/emfl_api',
+        firstName: 'Subrata',
+        lastName: 'Sarkar',
+        email: 'subrata@emfluence.com',
+        isAdmin: false
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+    fullNameUS(){
+      return `${this.user.lastName} ${this.user.firstName}`;
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if( newFollowerCount > oldFollowerCount ) {
+        //console.log('New follower added');
+        //Do something real here...
+      }
+    }
+  },
+
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  
+  mounted() {
+    this.followUser();
   }
 }
 </script>
@@ -19,8 +58,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.followers {
+    color:blueviolet;
+    font-weight: bold;
+}
+
+button {
+  width: auto;
 }
 </style>

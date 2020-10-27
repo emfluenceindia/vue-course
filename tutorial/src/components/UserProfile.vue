6ec132db94779b9e9e059fa14ead0e84bdf4acfc
Subrata Sarkar <ssarkar@emfluence.com>
2020-10-27 18:56:35 +0530

    Conditional Display
    Used v-if condition to display Admin badge. v-if / v-else-if / v-else can also be used on non-boolean fields like comparing a value range.

--- tutorial/src/components/UserProfile.vue
+++ tutorial/src/components/UserProfile.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="user-profile">
         <div class="user-profile__user-panel">
-            <h1 class="userprofile__username">{{user.username}}</h1>
+            <h1 class="userprofile__username">@{{user.username}}</h1>
+            <div class="user-profile__admin-badge" v-if="user.isAdmin">
+                Admin
+            </div>
+            <div class="user-profile__admin-badge" v-if="followers > 100">
+                100+ Followers
+            </div>
             <div class="user-profile__follower-count">
                 Followers: {{followers}}
             </div>
@@ -14,7 +20,7 @@ export default {
     name: 'UserProfile',
     data() {
         return {
-                followers: 0,
+                followers: 105,
                 user: {
                     id: 1,
                     username: 'emfl_api',
@@ -22,7 +28,7 @@ export default {
                     firstName: 'Subrata',
                     lastName: 'Sarkar',
                     email: 'subrata@emfluence.com',
-                    isAdmin: false
+                    isAdmin: true
                 }
             }
     },
@@ -72,19 +78,32 @@ export default {
     background-color: white;
     border-radius: 5px;
     border: 1px solid #666666;
+    text-align: left;
 }
 
 h1 {
     margin: 0;
     padding: 0;
-    font-size: 160%;
+    font-size: 150%;
     font-weight: normal;
     text-align: left;
 }
 
 .user-profile__follower-count {
+    margin: 5px 0;
     text-align: left;
     font-size: 84%;
+}
+
+.user-profile__admin-badge {
+    background-color: rebeccapurple;
+    color: white;
+    font-size: 80%;
+    margin-top: 5px;
+    margin-right: auto;
+    padding: 4px 10px;
+    border-radius: 3px;
+
 }
 
 
